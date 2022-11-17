e45398124ac7b766e890ffdf1fb709f762622a95
Nicolas Dos Santos <nicolas.dossantos@consulting-for.accor.com>
2022-11-17 22:14:28 +0100

    [serializer] Add unit tests

--- serializer/src/test/java/com/weedow/schemaorg/serializer/JsonLdSerializerImplTest.java
+++ serializer/src/test/java/com/weedow/schemaorg/serializer/JsonLdSerializerImplTest.java
@@ -6,12 +6,13 @@ import com.weedow.schemaorg.commons.model.JsonLdNode;
 import com.weedow.schemaorg.commons.model.JsonLdNodeImpl;
 import com.weedow.schemaorg.serializer.data.Example;
 import org.junit.jupiter.api.Test;
+import org.schema.model.*;
 import org.schema.model.datatype.Boolean;
-import org.schema.model.datatype.*;
 import org.schema.model.datatype.Float;
 import org.schema.model.datatype.Integer;
 import org.schema.model.datatype.Number;
-import org.schema.model.impl.ThingImpl;
+import org.schema.model.datatype.*;
+import org.schema.model.impl.*;
 
 import java.net.MalformedURLException;
 import java.time.LocalDate;
@@ -26,7 +27,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 class JsonLdSerializerImplTest {
 
     @Test
-    void serialize(@GivenTextResource("/data/JsonLdNodeImpl.json") String expected) throws JsonLdException {
+    void serialize(@GivenTextResource("/data/JsonLdNode.json") String expected) throws JsonLdException {
         final JsonLdSerializer jsonLdSerializer = new JsonLdSerializerImpl();
 
         JsonLdNode jsonLdNode = new JsonLdNodeImpl();
@@ -36,7 +37,7 @@ class JsonLdSerializerImplTest {
     }
 
     @Test
-    void serialize_with_pretty_print(@GivenTextResource("/data/JsonLdNodeImpl_pretty_print.json") String expected) throws JsonLdException {
+    void serialize_with_pretty_print(@GivenTextResource("/data/JsonLdNode_pretty_print.json") String expected) throws JsonLdException {
         final JsonLdSerializer jsonLdSerializer = new JsonLdSerializerImpl(JsonLdSerializerOptions.builder().prettyPrint(true).build());
 
         JsonLdNode jsonLdNode = new JsonLdNodeImpl();
@@ -69,16 +70,66 @@ class JsonLdSerializerImplTest {
     }
 
     @Test
-    void testSerialize(@GivenTextResource("/data/ThingImpl.json") String expected) throws JsonLdException, MalformedURLException {
+    void serialize_thing(@GivenTextResource("/data/Thing.json") String expected) throws JsonLdException, MalformedURLException {
         final JsonLdSerializer jsonLdSerializer = new JsonLdSerializerImpl(JsonLdSerializerOptions.builder().prettyPrint(true).build());
 
-        ThingImpl thing = new ThingImpl();
+        Thing thing = new ThingImpl();
         thing.setId("my_id");
         thing.setName(Text.of("My Thing"));
         thing.setDescription(Text.of("This is my thing."));
         thing.setUrl(URL.of(new java.net.URL("https://github.com/Kobee1203/schema-org-java")));
 
         String result = jsonLdSerializer.serialize(thing);
+
+        assertThatJson(result).isEqualTo(expected);
+    }
+
+    @Test
+    void serialize_complex_object(@GivenTextResource("/data/Hotel.json") String expected) throws JsonLdException, MalformedURLException {
+        final JsonLdSerializer jsonLdSerializer = new JsonLdSerializerImpl(JsonLdSerializerOptions.builder().prettyPrint(true).build());
+
+        Hotel hotel = new HotelImpl();
+        hotel.setName(Text.of("Sunny Hotel"));
+        hotel.setDescription(Text.of("This is a hotel where every day is sunny"));
+
+        Person employee = new PersonImpl();
+        employee.setGivenName(Text.of("John"));
+        employee.setFamilyName(Text.of("Doe"));
+        employee.setBirthDate(Date.of(LocalDate.of(1981, Month.MARCH, 12)));
+        employee.setEmail(Text.of("john.doe@sunnyhotel.com"));
+        hotel.setEmployee(employee);
+
+        PostalAddress postalAddress = new PostalAddressImpl();
+        postalAddress.setStreetAddress(Text.of("21 jump street"));
+        postalAddress.setAddressLocality(Text.of("Lost City"));
+        Country country = new CountryImpl();
+        country.setName(Text.of("United States Of America"));
+        country.setMap(URL.of(new java.net.URL("https://goo.gl/maps/tvsDMhfk3yah43Mm6")));
+        postalAddress.setAddressCountry(country);
+        hotel.setAddress(postalAddress);
+
+        Rating rating = new RatingImpl();
+        rating.setRatingValue(Number.of(4.5));
+        rating.setBestRating(Number.of(5));
+        rating.setWorstRating(Number.of(3.5));
+        hotel.setStarRating(rating);
+
+        OpeningHoursSpecification openingHoursSpecification = new OpeningHoursSpecificationImpl();
+        openingHoursSpecification.setOpens(Time.of(LocalTime.of(9, 0)));
+        openingHoursSpecification.setCloses(Time.of(LocalTime.of(23, 0)));
+        openingHoursSpecification.setDayOfWeek(DayOfWeekEnum.SATURDAY);
+        openingHoursSpecification.setValidFrom(DateTime.of(LocalDateTime.of(2022, Month.DECEMBER, 1, 9, 0)));
+        openingHoursSpecification.setValidThrough(Date.of(LocalDate.of(2024, Month.DECEMBER, 31)));
+        hotel.setOpeningHoursSpecification(openingHoursSpecification);
+
+        Event event = new EventImpl();
+        event.setName(Text.of("Funny Party"));
+        event.setEventAttendanceMode(EventAttendanceModeEnumerationEnum.MIXED_EVENT_ATTENDANCE_MODE);
+        event.setIsAccessibleForFree(Boolean.of(true));
+        event.setMaximumAttendeeCapacity(Integer.of(654));
+        hotel.setEvent(event);
+
+        String result = jsonLdSerializer.serialize(hotel);
 
         assertThatJson(result).isEqualTo(expected);
     }
